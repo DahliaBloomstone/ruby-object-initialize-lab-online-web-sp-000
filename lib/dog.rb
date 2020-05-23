d392ccbf2b8e690d25c7ddf752990e96c253e431
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-23 23:49:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,22 @@
-#step 1:
+#step 1: Initialize with attributes (name and breed)
 class Dog
 def initialize(name, breed = "Mutt")
 @name = name
 @breed = breed
 end
-#Step 2:
+
+#Step 2: Setter Method for name and breed
 def name=(name, breed = "Mutt")
 @name = name
 @breed = breed
 end
-#Step 3:
+
+#Step 3: Getter Method for name
 def name
 @name
 end
-#Step 4:
+
+#Step 4: Getter method for breed
 def breed
 @breed
 end
